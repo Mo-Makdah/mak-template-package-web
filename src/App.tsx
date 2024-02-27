d1877bfd1666ab6289c18094c3efac31d488bc7e
mo-makdah <mo.makdah@gmail.com>
2024-02-27 21:47:22 +0200

    - add tailwind - create color theme - create text color theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className="bg-primary">Primary</div>
+      <div className="bg-light">Primary Light</div>
+      <div className="bg-dark">Primary Dark</div>
+      <div className="bg-secondary">Secondary</div>
+      <div className="bg-gray-primary">Grey</div>
+      <div className="bg-gray-light">Light Grey</div>
+      <div className="bg-gray-dark">Dark Grey</div>
+      <div className="bg-black">Black</div>
+      <div className="bg-white">White</div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#08D9D6",
+        light: "#EAEAEA",
+        dark: "#252A34",
+        secondary: "#FF2E63",
+        gray: {
+          primary: "#808080",
+          light: "#D3D3D3",
+          dark: "#A9A9A9",
+        },
+        black: "#000000",
+        white: "#FFFFFF",
+      },
+      textColor: {
+        primary: "#252A34",
+        secondary: "#EAEAEA",
+      },
+    },
   },
   plugins: [],
-}
-
+};
